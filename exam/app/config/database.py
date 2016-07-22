7a6dc221ef1ec41f85fd0e973ca38202a92d1da7
jkspam2004 <jkspam2004@yahoo.com>
2016-07-22 13:13:40 -0700

    adding exam

---
+++ exam/app/config/database.py
@@ -0,0 +1,34 @@
+import os
+
+class DBConfig(object):
+    DB_ON = True
+    DB_DRIVER = 'mysql'
+    DB_ORM = False
+
+class DevelopmentDBConfig(DBConfig):
+    DB_USERNAME = 'root'
+    DB_PASSWORD = 'root'
+    DB_DATABASE_NAME = 'examdb'
+    DB_HOST = 'localhost'
+    DB_PORT = 3306
+    DB_OPTIONS = {
+        'unix_socket' : '/Applications/MAMP/tmp/mysql/mysql.sock'
+    } 
+class StagingDBConfig(DBConfig):
+    DB_USERNAME = 'root'
+    DB_PASSWORD = 'root'
+    DB_DATABASE_NAME = 'examdb'
+    DB_HOST = 'localhost'
+    DB_PORT = 3306
+    DB_OPTIONS = {
+        'unix_socket' : '/Applications/MAMP/tmp/mysql/mysql.sock'
+    } 
+class ProductionDBConfig(DBConfig):
+    DB_USERNAME = 'root'
+    DB_PASSWORD = 'root'
+    DB_DATABASE_NAME = 'examdb'
+    DB_HOST = 'localhost'
+    DB_PORT = 3306
+    DB_OPTIONS = {
+        'unix_socket' : '/Applications/MAMP/tmp/mysql/mysql.sock'
+    } 
