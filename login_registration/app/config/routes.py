86c36c69b90dd79722109e0eed4ba764e13fb337
jkspam2004 <jkspam2004@yahoo.com>
2016-08-02 01:27:52 -0700

    remove .pyc files from full friends

--- login_registration/app/config/routes.py
+++ login_registration/app/config/routes.py
@@ -1,6 +1,6 @@
 from system.core.router import routes
 
-routes['default_controller'] = 'Logins'
+routes['default_controller']      = 'Logins'
 routes['POST']['/users/register'] = 'Logins#register'
-routes['POST']['/users/login'] = 'Logins#login'
-routes['/success'] = 'Logins#success'
+routes['POST']['/users/login']    = 'Logins#login'
+routes['/success']                = 'Logins#success'
